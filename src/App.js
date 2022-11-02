74f568daf287c83623b42c7436d140388421911e
Sharkri <sharkdude955@gmail.com>
2022-11-02 18:42:51 -0500

    Add uniqid key to mapping

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import "./styles/App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import countries from "./countries.json";
+import uniqid from "uniqid";
 
 function App() {
   const getRandomFlag = () => {
     const country = countries[Math.floor(Math.random() * countries.length)];
     const flag = require(`./images/${country.code.toLowerCase()}.png`);
 
-    return { image: flag, name: country.name };
+    return { image: flag, name: country.name, id: uniqid() };
   };
 
   const [currentScore, setCurrentScore] = useState(0);
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ export default function Main({ cards }) {
   return (
     <main>
       {cards.map((card) => (
-        <Card image={card.image} name={card.name} />
+        <Card image={card.image} name={card.name} key={card.id} />
       ))}
     </main>
   );
