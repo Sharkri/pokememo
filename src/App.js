cb0eb014141f56ede271542f9c77fdbfe53f53d1
Sharkri <sharkdude955@gmail.com>
2022-11-01 20:30:02 -0500

    add scoreboard to header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import "./styles/App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
 function App() {
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
   return (
     <div className="App">
-      <Header />
+      <Header currentScore={currentScore} bestScore={bestScore} />
       <Main />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,9 @@
-export default function Header() {
-  return <header></header>;
+export default function Header({ currentScore, bestScore }) {
+  return (
+    <header>
+      <h1>Memory Game</h1>
+      <p>Score: {currentScore}</p>
+      <p>Best Score: {bestScore}</p>
+    </header>
+  );
 }
