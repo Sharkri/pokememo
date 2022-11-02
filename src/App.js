cc3f852c9f7de44bbec662acf97628ac7a6f6c38
Sharkri <sharkdude955@gmail.com>
2022-11-02 05:09:57 -0500

    Add random flag generator

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import { useState } from "react";
 import "./styles/App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import flags from "./flags.json";
 
 function App() {
+  const getRandomFlag = () => flags[Math.floor(Math.random() * flags.length)];
+
   const [currentScore, setCurrentScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
-  const [cards, setCards] = useState([]);
+  const [cards, setCards] = useState([getRandomFlag()]);
 
   return (
     <div className="App">
