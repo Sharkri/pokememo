0ede45706166d14156576e6b756029ffdfc71b84
Sharkri <sharkdude955@gmail.com>
2022-11-01 20:21:39 -0500

    exported components to main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./styles/App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+    </div>
+  );
 }
 
 export default App;
