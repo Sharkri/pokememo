47b332f0eced94d04877f959ba63a04058b2b228
Sharkri <sharkdude955@gmail.com>
2022-11-02 20:09:30 -0500

    Make image undraggable

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,12 @@ import "../styles/Card.css";
 export default function Card({ image, name }) {
   return (
     <div className="card">
-      <img src={image} alt={`Flag of ${name}`} className="flag" />
+      <img
+        src={image}
+        alt={`Flag of ${name}`}
+        className="flag"
+        draggable="false"
+      />
       <p className="card-name">{name}</p>
     </div>
   );
