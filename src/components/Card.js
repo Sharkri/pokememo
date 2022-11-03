e46a0cb695c5d2444e2c8b6a387473bb3b064418
Sharkri <sharkdude955@gmail.com>
2022-11-03 02:57:48 -0500

    Change card from div to button

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import "../styles/Card.css";
 
 export default function Card({ image, name, onClick, index }) {
   return (
-    <div className="card" onClick={onClick} data-index={index}>
+    <button className="card" onClick={onClick} data-index={index}>
       <img
         src={image}
         alt={`Flag of ${name}`}
@@ -10,6 +10,6 @@ export default function Card({ image, name, onClick, index }) {
         draggable="false"
       />
       <p className="card-name">{name}</p>
-    </div>
+    </button>
   );
 }
