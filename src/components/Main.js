6c8e1e90400a63661359983712071f1ea32fa65e
Sharkri <sharkdude955@gmail.com>
2022-11-03 03:08:13 -0500

    Add game instructions

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,15 +4,21 @@ import "../styles/Main.css";
 export default function Main({ cards, onClick }) {
   return (
     <main>
-      {cards.map((card, index) => (
-        <Card
-          image={card.image}
-          name={card.name}
-          key={card.id}
-          index={index}
-          onClick={onClick}
-        />
-      ))}
+      <p className="game-instructions">
+        Get points by clicking on a country but don't click on any more than
+        once.
+      </p>
+      <div className="cards">
+        {cards.map((card, index) => (
+          <Card
+            image={card.image}
+            name={card.name}
+            key={card.id}
+            index={index}
+            onClick={onClick}
+          />
+        ))}
+      </div>
     </main>
   );
 }
