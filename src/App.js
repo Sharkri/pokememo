d444e3b32352510f6d7957f8cff3ffb36d6ee628
Sharkri <sharkdude955@gmail.com>
2022-11-02 23:17:56 -0500

    Change random country function to generate multiple countries in an array

--- src/App.js
+++ src/App.js
@@ -6,21 +6,19 @@ import countries from "./countries.json";
 import uniqid from "uniqid";
 
 function App() {
-  const getRandomFlag = () => {
-    const country = countries[Math.floor(Math.random() * countries.length)];
-    const flag = require(`./images/${country.code.toLowerCase()}.png`);
-
-    return { image: flag, name: country.name, id: uniqid() };
+  const pickRandomCountries = (number) => {
+    const array = [];
+    for (let i = 0; i < number; i++) {
+      const country = countries[Math.floor(Math.random() * countries.length)];
+      const flag = require(`./images/${country.code.toLowerCase()}.png`);
+      array.push({ image: flag, name: country.name, id: uniqid() });
+    }
+    return array;
   };
 
   const [currentScore, setCurrentScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
-  const [cards, setCards] = useState([
-    getRandomFlag(),
-    getRandomFlag(),
-    getRandomFlag(),
-    getRandomFlag(),
-  ]);
+  const [cards, setCards] = useState(pickRandomCountries(4));
 
   return (
     <div className="App">
