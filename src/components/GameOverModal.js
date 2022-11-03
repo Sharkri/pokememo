d76cb1270902952785a8f6162a0a902722870fa8
Sharkri <sharkdude955@gmail.com>
2022-11-03 03:10:58 -0500

    Change game over wording

--- src/components/GameOverModal.js
+++ src/components/GameOverModal.js
@@ -4,8 +4,8 @@ export default function GameOverModal({ score, onPlayAgain }) {
   return (
     <div className="game-over-modal">
       <div className="game-over-content">
-        <h2>Game over!</h2>
-        <p className="final-score">Your score is {score}</p>
+        <h2>Game Over!</h2>
+        <p className="final-score">Your final score is {score}</p>
         <button className="play-again" onClick={onPlayAgain}>
           Play again
         </button>
