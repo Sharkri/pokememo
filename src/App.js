72a7bb00aca5e26a7e3f9e10c114e858ffa6f973
Sharkri <sharkdude955@gmail.com>
2022-11-03 21:24:53 -0500

    Remove unused import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./styles/App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
