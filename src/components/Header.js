6965006a23af7ab69362b60c2a7137bee7f16739
Sharkri <sharkdude955@gmail.com>
2023-07-18 13:48:06 -0500

    Show best level reached and add logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,24 @@
 import "../styles/Header.css";
 
-export default function Header({ currentScore, bestScore, level }) {
+export default function Header({ currentScore, bestScore, level, bestLevel }) {
   return (
     <header>
-      <h1>Memory Game</h1>
+      <h1>
+        <img
+          src={`${process.env.PUBLIC_URL}/pokeball.png`}
+          alt="pokeball"
+          className="pokeball"
+        />
+        <span>
+          <span class="poke">Poké</span>
+          <span className="memo">Memo</span>
+        </span>
+      </h1>
       <div className="stats">
         <p className="current-score">Score: {currentScore}</p>|
         <p className="best-score">Best Score: {bestScore}</p>|
-        <p className="level">Level {level}</p>
+        <p className="level">Level {level}</p>|
+        <p className="level">Best Level Reached: {bestLevel}</p>
       </div>
     </header>
   );
